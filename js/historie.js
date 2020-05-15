df5004854296e2a5fa47b8ebba289b62be5ffb9d
jahnisko <vitezslav.jahn@post.cz>
2020-05-15 17:47:37 +0200

    jahnisko

---
+++ js/historie.js
@@ -0,0 +1,186 @@
+const udalosti = [
+    {
+        "title": "Vznik Svaté říše římské",
+        "year": "800",
+        "description": "Svatá říše římská je název zaniklého mnohonárodnostního svazku mnoha politických útvarů, který se rozkládal ve střední Evropě. 25. prosince 800 papež Lev III. korunoval franského krále Karla Velikého na císaře, který se stal na území západní Evropy prvním císařem po více než 300 letech. Až do roku 899 tento titul získávali členové dynastie Karla Velikého, Karlovci. Poté je vystřídali členové různých italských dynastií až do roku 924. Pak byl trůn neobsazený do roku 962, kdy byl na císaře korunován Ota I. Veliký, který sám sebe označoval za pokračovatele Karla Velikého. Krále Svaté říše římské vybírali volitelé, kurfiřti. Zvolený panovník se pak musel nechat papežem korunovat na císaře. V 16. století byla povinnost korunovace papežem zrušena.",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "Reformace",
+        "year": "1517",
+        "description": "Počátek reformace se datuje do 31. října 1517, kdy Martin Luther zveřejnil na dveřích kostela ve Wittenbergu 95 tezí, v nichž se vyslovil proti odpustkům a jiným nešvarům v církvi. O čtyři roky později byl za to z církve vyloučen, nicméně reformace se v té době už nezadržitelně šířila Německem.",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "Vznik Německého císařství",
+        "year": "1871",
+        "description": "Německé císařství byl německý národní stát, který existoval od sjednocení Německa v roce 1871 až do abdikace císaře Viléma II. v roce 1918. Bylo založeno 1. ledna 1871, kdy se německé jižní státy, s výjimkou Rakouska, připojily k Severoněmeckému spolku a vstoupila v platnost nová ústava",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "První světová válka",
+        "year": "1914",
+        "description": "Roku 1914 vypukla 1. světová válka. Podnětem k ní se stalo zavraždění rakouského následníka trůnu, arcivévody Františka Ferdinanda, srbským nacionalistou v bosenském Sarajevu 28. června 1914. Mezi skutečné příčiny je zahrnována německo-francouzská animozita, obtížné soužití národů v mnohonárodnostním Rakousku-Uhersku, ruská agresivní politika na Balkáně a ukvapené stanovování ultimát a provádění mobilizací (v důsledku mylného přesvědčení, že eventuální válka bude krátká). Po boku Německa bojovalo Rakousko-Uhersko, Belgie a Osmanská říše. Válka skončila podepsáním příměří ve francouzském Compiégne.",
+        "destruction": "true",
+        "battle": "true"
+    },
+    {
+        "title": "Vznik Výmarské republiky",
+        "year": "1918",
+        "description": "Výmarská republika je označení pro historický stát Německa po pádu monarchií v Německu v roce 1918 až do nástupu nacistů k moci v roce 1933. Hlavou státu byl říšský prezident, zákonodárným sborem dvoukomorový parlament (Národní shromáždění) skládající se z Říšského sněmu a Říšské rady. V čele exekutivy stála říšská vláda vedená říšským kancléřem, kterého jmenoval říšský prezident.",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "Nacistické německo",
+        "year": "1933",
+        "description": "Jako nacistické Německo se označuje německý stát v době vlády nacistické strany, tedy od roku 1933 (jmenování Adolfa Hitlera německým kancléřem proběhlo 30. ledna 1933 a diktátorské pravomoci Hitler převzal v březnu 1933) do kapitulace Německa na konci druhé světové války 8. května 1945. Oficiální název státu byl Německá říše (německy Deutsches Reich) do roku 1943 a Velkoněmecká říše (německy Großdeutsches Reich) od roku 1943 do jeho zániku roku 1945. K dalším užívaným označením nacistického Německa patří pojmy třetí říše, „národně-socialistický stát“ či „vůdcovský stát“. Adolf Hitler se 30. ledna 1933 dostal legálně k moci, když byl dosazen do funkce říšského kancléře prezidentem tehdejšího Německa Paulem von Hindenburgem.",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "2. světová válka",
+        "year": "1939",
+        "description": "1. září 1939 vypukla německým přepadením Polska druhá světová válka. O dva dny později vyhlásily Velká Británie a Francie Německu válku. Poláci se statečně bránili, ovšem proti německé bleskové válce se ukázali zcela bezmocní. Po pouhém měsíci válčení bylo Polsko na kolenou. 22. června 1941 zaútočilo Německo na Sovětský svaz. Po několika měsících tohoto mohutného tažení padlo do německých rukou celé západní Rusko, Ukrajina, Bělorusko a Pobaltí. V zimě 1941/1942 ale německý postup zamrzl před Moskvou. Druhá světová válka v Evropě skončila 8. května 1945, kdy vstoupila v platnost bezpodmínečná kapitulace Německa.",
+        "destruction": "true",
+        "battle": "true"
+    },
+    {
+        "title": "Rozdělení poválečného Německa, okupační pásma",
+        "year": "1945",
+        "description": "V květnu 1945 se Německo ocitlo v hodině nula. Německá města byla zničená leteckým bombardováním, země zaplavená uprchlíky, obyvatelé strádali hladem. Vítězné mocnosti USA, Velká Británie a SSSR se v červenci 1945 sešly na postupimské konferenci, kde se usnesly na demokratizaci, denacifikaci a demilitarizaci Německa. Země byla rozdělena do čtyř okupačních pásem a podřízena spojenecké kontrolní radě. Spojení tří západních sektorů a přijetí ústavy vedlo 23. května 1949 ke vzniku Spolkové republiky Německo („západní Německo“), na což Sověti zareagovali 7. října téhož roku založením Německé demokratické republiky („východní Německo“).",
+        "destruction": "true",
+        "battle": "false"
+    },
+    {
+        "title": "Znovusjednocení Německa",
+        "year": "po roce 1990",
+        "description": "Pád Berlínské zdi roku 1989 předpověděl příznivou budoucnost celého Německa. Německé znovusjednocení byl proces připojení Německé demokratické republiky včetně Východního Berlína ke Spolkové republice Německo, završený 3. října 1990. Bylo vyvrcholením dlouhodobých snah západoněmeckých politiků a veřejnosti o překonání poválečného uspořádání Německa. Velkou měrou k němu přispěly také politické změny v NDR samotné a vývoj v Sovětském svazu a ostatních státech tzv. komunistického bloku. Znamenalo definitivní konec rozdělení Německa a znovuobnovení německé jednoty, ovšem v hranicích, které byly stanoveny vítěznými mocnostmi druhé světové války.",
+        "destruction": "false",
+        "battle": "false"
+    }
+];
+const osobnosti = [
+    {
+        "name": "Johannes Gutenberg",
+        "born": "kolem roku 1400",
+        "died": "3. února 1468",
+        "description": "Johannes Gensfleisch, řečený Gutenberg, byl vynálezce technologie mechanického knihtisku pomocí sestavovatelných liter. Důsledkem rozšíření jeho objevů byla masová produkce knih a přelom v možnostech šíření informací. Svůj vynález uskutečnil bez znalostí tiskových technologií Dálného východu.",
+        "image": "../img/gutenberg.jpg"
+    },
+    {
+        "name": "Martin Luther",
+        "born": "10. listopadu 1483",
+        "died": "18. února 1546",
+        "description": "Martin Luther byl německý teolog, kazatel a reformátor, zakladatel protestantismu, autor řady duchovních, politických, pedagogických spisů, církevních písní a překladů. Jeho nejvýznamnějším dílem je překlad Bible do němčiny.",
+        "image": "../img/luther.jpg"
+    },
+    {
+        "name": "Wolfgang Amadeus Mozart",
+        "born": "27. ledna 1756",
+        "died": "5. prosince 1791",
+        "description": "Wolfgang Amadeus Mozart byl rakouský klasicistní hudební skladatel a klavírní virtuos. Je uznáván jako geniální hudebník, který za svého života složil 626 děl světského i duchovního charakteru – opery, symfonie, koncertní skladby pro různé sólové nástroje, komorní hudbu, mše a chorály. Ačkoliv se Amadeus narodil v Salzburgu (dnešní Rakousko), sám se považoval za Němce, neboť Salzburg v té době patřil do Svaté říše římské národa německého. Rovněž jeho otec byl Němec.",
+        "image": "../img/mozart.jpg"
+    },
+    {
+        "name": "Friedrich Schiller",
+        "born": "10. listopadu 1759",
+        "died": "9. května 1805",
+        "description": "Johann Christoph Friedrich von Schiller byl německý spisovatel, básník, dramatik, estetik a historik. Byl vedoucí představitel německé klasiky, čestný občan Francouzské republiky. Jeho dramatické dílo patetickým způsobem hájí svobodu a práva člověka a lidské bratrství. Jeho názory se utvářely pod vlivem Rousseaua, Lessinga a hnutí Sturm und Drang. V estetice vychází z Kanta. Umění považoval za prostředek formování harmonické osobnosti, která svobodně tvoří dobro. Podle něj pouze umění pomáhá člověku získat skutečnou svobodu.",
+        "image": "../img/schiller.jpg"
+    },
+    {
+        "name": "Otto von Bismarck",
+        "born": "1. dubna 1815",
+        "died": "30. července 1898",
+        "description": "Otto Eduard Leopold von Bismarck-Schönhausen, vévoda z Lauenburgu byl jeden z nejvýznamnějších politiků 19. století a budovatel sjednoceného Německa. Byl první ministr (tj. premiér) Pruska (1862–1890) a první v řadě německých kancléřů (1871–1890). Jeho nekompromisní přístup k řešení politických problémů a autoritativní vystupování vůči sněmu mu vyneslo přezdívku Železný kancléř.",
+        "image": "../img/bismarck.jpg"
+    },
+    {
+        "name": "Albert Einstein",
+        "born": "14. března 1879",
+        "died": "18. dubna 1955",
+        "description": "Albert Einstein byl teoretický fyzik, jeden z nejvýznamnějších vědců všech dob. Často je označován za největšího vědce 20. století, případně spolu s Newtonem za nejvýznamnějšího fyzika vůbec. Mezi jeho příspěvky fyzice patří speciální teorie relativity (1905), myšlenka kvantování elektromagnetického pole a vysvětlení fotoefektu (1905), vysvětlení Brownova pohybu (1905) a snad nejvíce obecná teorie relativity (1915), která doposud nejlépe popisuje vesmír ve velkých měřítkách.",
+        "image": "../img/einstein.jpg"
+    },
+];
+const literatura = [
+    {
+        "autor" : "neznámý autor",
+        "url" : "https://cs.wikipedia.org/wiki/P%C3%ADse%C5%88_o_Nibelunz%C3%ADch",
+        "dilo" : "Píseň o Nibelunzích",
+        "nemecky" : "Das Nibelungenlied",
+        "vydani" : "13. století",
+        "images" : [
+            "../img/pisen_o_nibelunzich.jpg",
+            "../img/pisen_o_nibelunzich_1.jpg"
+        ],
+        "popis" : "Píseň o Nibelunzích je dvoudílný středověký hrdinský epos, který vznikl ve 13. století a je napsán tehdejším lidovým jazykem – střední horní němčinou. Píseň o Nibelunzích je napsána ve zpěvných slokách rozdělených do čtyř řádků, jejich melodii však není možno s jistotou zrekonstruovat. Epos se skládá ze dvou částí: v první části stojí ve středu pozornosti Siegfriedova vražda, ve druhé pak pomsta jeho manželky Kriemhildy. Děj se odehrává v okolí Burgundské říše na Rýně a také (ve druhém díle) v jižním Německu a kolem Dunaje v dnešním Rakousku a Maďarsku. Obě části díla se k sobě mají jako příčina a následek. Historickým jádrem tohoto eposu je zničení Burgundské říše u Wormsu v pozdní antice."
+    },
+    {
+        "autor" : "Heinrich Heine",
+        "url" : "https://cs.wikipedia.org/wiki/Heinrich_Heine",
+        "dilo"  : "Kniha písní",
+        "nemecky" : "Buch der Lieder",
+        "vydani" : "1824",
+        "images" : [
+            "../img/lorelei.png",
+            "../img/kniha_pisni.jpg"
+        ],
+        "popis" : "Básnická sbírka z období romantismu, Kniha písní, vychází z lidové poezie a tvoří ji jednoduché melodické verše. Sbírku prostupují témata jako zhrzená láska, erotika a světobol. Nešťastné lásky se dotýká slavná báseň Lorelei, která postupem času zlidověla.",
+    }, 
+    {
+        "autor" : "Jacob a Wilhelm Grimmovi",
+        "url" : "https://cs.wikipedia.org/wiki/Brat%C5%99i_Grimmov%C3%A9",
+        "dilo" : "Pohádky bratří Grimmů",
+        "nemecky" : "Kinder- und Hausmärchen",
+        "vydani" : "1812",
+        "images" : [
+            "../img/grimms.jpg",
+            "../img/grimms_1.jpg"
+        ],
+        "popis" : "Pohádky bratří Grimmů byly publikovány ve dvou svazcích (1812–1815), poslední edice vyšla v roce 1857 a obsahovala 211 pohádek, dalších 28 bylo v dřívějších edicích, takže celkem tedy sbírka obsahovala 239 pohádkových příběhů. Pohádky byly zapsány podle ústní tradice, v prvním vydání bylo použito 10 dialektů německého jazyka. Některé pohádky se staly proslulými po celém světě, jsou půvabné svým kouzlem, motivy komunikace mezi lidmi a zvířaty a morálním patosem boje dobra se zlem. Mezi nejznámější publikované pohádky patří Sněhurka, Červená karkulka, Šípková Růženka či Jeníček a Mařenka."
+    }
+];
+$(function () {
+    $("h4").on("click", function () {
+        $(this).next().toggle(1500);
+        $(".card-title").next().empty();
+    });
+    $(".toglik").on("click", function(){
+        $(this).parents(".row").next().toggle(1500);
+    })
+    udalosti.forEach((item) => {
+        $("#udalosti").append(`<div class="row border p-3 bg-dark text-warning m-0 fontik"><div class="col-md-4 border-right"><p>${item.year}</p></div><div class="col-md-6 border-right title"><p><i class="fas fa-chevron-down"></i> ${item.title}</p><p class="des">${item.description}</p></div><div class="col-md-2"><p>${(item.destruction == "false") ? '<i class="fas fa-check text-warning"></i>' : '<i class="fas fa-times"></i>'}</p></div></div>`);
+        $(".des").hide();
+        $(".title").on("click", function () {
+            $(".des").hide();
+            $(this).children().show(300);
+        });
+    });
+    osobnosti.forEach((osobnost) => {
+        $("#karty .row").append(`<div class="card ml-5 mb-3 border-warning" style="width: 18rem;"><img class="card-img-top" src="${osobnost.image}" alt="Osobnost" height="300"><div class="card-body"><h5 class="card-title">${osobnost.name}</h5><div class="information"></div></div>`);
+    });
+    $(".card-title").on("click", function(){
+        $(this).next().empty();
+        let jmeno = $(this).text();
+        let osobnost = osobnosti.find(item => {return item.name === jmeno});
+        $(this).next().append(`<table class='table table-bordered'><tbody><tr><td class='font-weight-bold'>Datum narození</td><td>${osobnost.born}</td></tr><tr><td class='font-weight-bold'>Datum úmrtí</td><td>${osobnost.died}</td></tr></tbody></table><p class='blok'>${osobnost.description}<p>`).hide().show(2000);
+    });
+    literatura.forEach((pismo)=>{
+        $("#literatura .row").append(`
+        <div class="col-md-4">
+            <img src="${pismo.img}" height="280" alt="Německá historická literární díla">
+            <h3 class="font-weight-bold">${pismo.dilo}</h3>
+            <h5><i>${pismo.autor}</i></h5>
+        </div>`)
+    })
+    //$("")
+    /*Zde bych chtěl vyplnit vždy po kliklnutí na osobu "colik", tzn. informace o osobnosti*/
+    /* 1. Musíte zajistit kliknutí na konkrétní odrážku obsahující jméno osobnosti*/
+
+
+});
